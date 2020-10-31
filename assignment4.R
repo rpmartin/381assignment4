600585fd61af40ee44cf90da22238cd21ecc0386
rich <rich.p.martin@gmail.com>
2020-10-31 08:34:37 -0700

    fixed typos

--- assignment4.R
+++ assignment4.R
@@ -1,6 +1,6 @@
 ###### This is the file where you save your R code that produces the objects (graphs, tables) that you want to embed into 
-# your assignment.  Make sure that there are no errors when you source this file before sourcing it from assignment3.Rmd file.
-# To show you how it is done I have included code below that answers question 3.
+# your assignment.  Make sure that there are no errors when you source this file before sourcing it from assignment4.Rmd file.
+# To show you how it is done I have included code below that answers question 7.
 
 rm(list=ls()) #makes sure that your work environment is clean.
 library("tidyverse") #we use functions from this library.
